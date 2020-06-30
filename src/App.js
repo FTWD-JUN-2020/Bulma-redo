b69ed4d1574733187bd43e8a2da071cbaeeb1316
Cule219 <culaficstefan@gmail.com>
2020-06-30 09:43:59 -0400

    first 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./navbar/Navbar";
+import FormField from "./formfield/FormField";
+
+import "bulma/css/bulma.css";
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  let data = "Valerie";
+
+  return (
+    <div>
+      <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      <FormField label="IH" type="number" placeholder="e.g IH" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+    </div>
+  );
 };
 
 export default App;
